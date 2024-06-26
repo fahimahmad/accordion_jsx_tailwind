e18c5c9dcbbbdea9fbb3637d363119dbb3425e26
fahimahmad <fahim.ahmad053@gmail.com>
2024-06-26 13:05:21 +0500

    accordion added

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Accordion from "./components/(Accordion)/Accordion";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Accordion/>
+    </main>
+  );
+}
